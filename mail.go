fe23036e727c1b2eef300af7540bef575be238b8
Samuel Ventura <samuel.ventura@yeico.com>
2021-10-03 02:08:10 -0500

    smtp timeout

---
+++ mail.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net"
+	"net/mail"
+	"sort"
+	"strings"
+)
+
+//with display name
+//Jane Doe <jane.doe@gmail.com>
+//dig google.com MX
+
+func sendText(dao Dao, from string, to string, subject string, body string) error {
+	return sendEmail(dao, from, to, subject, "text/plain", body)
+}
+
+func sendEmail(dao Dao, from string, to string, subject string, mime string, body string) error {
+	fromAddress, err := mail.ParseAddress(from)
+	if err != nil {
+		return err
+	}
+	toAddress, err := mail.ParseAddress(to)
+	if err != nil {
+		return err
+	}
+	toDomain := strings.Split(toAddress.Address, "@")[1]
+	fromDomain := strings.Split(fromAddress.Address, "@")[1]
+	fromDomainDro, err := dao.GetDomainPrivateKey(fromDomain)
+	if err != nil {
+		return err
+	}
+	email, bodyLength := composeMimeMail(toAddress.String(), fromAddress.String(), subject, mime, body)
+	err = dkimSign(&email, bodyLength, fromDomain, []byte(fromDomainDro.PrivateKey))
+	if err != nil {
+		return err
+	}
+	//log.Println(string(email))
+	mxs, err := net.LookupMX(toDomain)
+	if err != nil {
+		return err
+	}
+	sort.Slice(mxs, func(i, j int) bool {
+		return mxs[i].Pref < mxs[j].Pref
+	})
+	for _, x := range mxs {
+		//log.Println(x.Host, x.Pref)
+		addr := fmt.Sprintf("%s:25", x.Host)
+		dial := false
+		err = smtpSend(addr, fromAddress.Address, []string{toAddress.Address}, email, &dial)
+		if dial {
+			continue
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return fmt.Errorf("no working mx %v", mxs)
+}
+
+func encodeRFC2047(str string) string {
+	// use mail's rfc2047 to encode any string
+	addr := mail.Address{Name: str}
+	return strings.Trim(addr.String(), " <>")
+}
+
+func composeMimeMail(to string, from string, subject string, mime string, body string) ([]byte, uint) {
+	var b strings.Builder
+	fmt.Fprintf(&b, "%s: %s\r\n", "From", from)
+	fmt.Fprintf(&b, "%s: %s\r\n", "To", to)
+	fmt.Fprintf(&b, "%s: %s\r\n", "Subject", encodeRFC2047(subject))
+	fmt.Fprintf(&b, "%s: %s\r\n", "MIME-Version", "1.0")
+	fmt.Fprintf(&b, "%s: %s\r\n", "Content-Type", fmt.Sprintf("%s; charset=\"utf-8\"", mime))
+	fmt.Fprintf(&b, "%s: %s\r\n", "Content-Transfer-Encoding", "base64")
+	bytes := []byte(body)
+	b.WriteString("\r\n")
+	b.WriteString(base64.StdEncoding.EncodeToString(bytes))
+	return []byte(b.String()), uint(len(bytes))
+}
--- smtp.go
+++ smtp.go
@@ -1,123 +1,375 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package smtp implements the Simple Mail Transfer Protocol as defined in RFC 5321.
+// It also implements the following extensions:
+//	8BITMIME  RFC 1652
+//	AUTH      RFC 2554
+//	STARTTLS  RFC 3207
+// Additional extensions may be handled by clients.
+//
+// The smtp package is frozen and is not accepting new features.
+// Some external packages provide more functionality. See:
+//
+//   https://godoc.org/?q=smtp
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
-	"log"
+	"crypto/tls"
+	"errors"
+	"io"
 	"net"
-	"net/mail"
-	"net/smtp"
-	"sort"
+	"net/textproto"
 	"strings"
+	"time"
 )
 
-//with display name
-//Jane Doe <jane.doe@gmail.com>
-//dig google.com MX
-
-func sendText(dao Dao, from string, to string, subject string, body string) error {
-	return sendEmail(dao, from, to, subject, "text/plain", body)
+// A Client represents a client connection to an SMTP server.
+type Client struct {
+	// Text is the textproto.Conn used by the Client. It is exported to allow for
+	// clients to add extensions.
+	Text *textproto.Conn
+	// keep a reference to the connection so it can be used to create a TLS
+	// connection later
+	conn net.Conn
+	// whether the Client is using TLS
+	tls        bool
+	serverName string
+	// map of supported extensions
+	ext map[string]string
+	// supported auth mechanisms
+	auth       []string
+	localName  string // the name to use in HELO/EHLO
+	didHello   bool   // whether we've said HELO/EHLO
+	helloError error  // the error from the hello
 }
 
-func sendEmail(dao Dao, from string, to string, subject string, mime string, body string) error {
-	fromAddress, err := mail.ParseAddress(from)
+// smtpDial returns a new Client connected to an SMTP server at addr.
+// The addr must include a port, as in "mail.example.com:smtp".
+func smtpDial(addr string) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	toAddress, err := mail.ParseAddress(to)
+	host, _, _ := net.SplitHostPort(addr)
+	return NewClient(conn, host)
+}
+
+// NewClient returns a new Client using an existing connection and host as a
+// server name to be used when authenticating.
+func NewClient(conn net.Conn, host string) (*Client, error) {
+	text := textproto.NewConn(conn)
+	_, _, err := text.ReadResponse(220)
 	if err != nil {
-		return err
+		text.Close()
+		return nil, err
 	}
-	// hostname, err := os.Hostname()
-	// if err != nil {
-	// 	return err
-	// }
-	toDomain := strings.Split(toAddress.Address, "@")[1]
-	fromDomain := strings.Split(fromAddress.Address, "@")[1]
-	fromDomainDro, err := dao.GetDomainPrivateKey(fromDomain)
-	if err != nil {
-		return err
-	}
-	email, bodyLength := composeMimeMail(toAddress.String(), fromAddress.String(), subject, mime, body)
-	err = dkimSign(&email, bodyLength, fromDomain, []byte(fromDomainDro.PrivateKey))
-	if err != nil {
-		return err
-	}
-	log.Println(string(email))
-	mxs, err := net.LookupMX(toDomain)
-	if err != nil {
-		return err
-	}
-	sort.Slice(mxs, func(i, j int) bool {
-		return mxs[i].Pref < mxs[j].Pref
-	})
-	for _, x := range mxs {
-		log.Println(x.Host, x.Pref)
-	}
-	err = nil
-	for _, x := range mxs {
-		addr := fmt.Sprintf("%s:25", x.Host)
-		// conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
-		// if err != nil {
-		// 	continue
-		// }
-		// c, err := smtp.NewClient(conn, hostname)
-		// if err != nil {
-		// 	return "", err
-		// }
-		// defer c.Quit()
-		//FIXME add timeout
-		err = smtp.SendMail(addr, nil, fromAddress.Address, []string{toAddress.Address}, email)
-		if err == nil {
-			break
+	c := &Client{Text: text, conn: conn, serverName: host, localName: "localhost"}
+	_, c.tls = conn.(*tls.Conn)
+	return c, nil
+}
+
+// Close closes the connection.
+func (c *Client) Close() error {
+	return c.Text.Close()
+}
+
+// hello runs a hello exchange if needed.
+func (c *Client) hello() error {
+	if !c.didHello {
+		c.didHello = true
+		err := c.ehlo()
+		if err != nil {
+			c.helloError = c.helo()
 		}
-		//FIXME retry only if network error
-		log.Println(x.Host, x.Pref, err)
-		break
 	}
+	return c.helloError
+}
+
+// Hello sends a HELO or EHLO to the server as the given host name.
+// Calling this method is only necessary if the client needs control
+// over the host name used. The client will introduce itself as "localhost"
+// automatically otherwise. If Hello is called, it must be called before
+// any of the other methods.
+func (c *Client) Hello(localName string) error {
+	if err := validateLine(localName); err != nil {
+		return err
+	}
+	if c.didHello {
+		return errors.New("smtp: Hello called after other methods")
+	}
+	c.localName = localName
+	return c.hello()
+}
+
+// cmd is a convenience function that sends a command and returns the response
+func (c *Client) cmd(expectCode int, format string, args ...interface{}) (int, string, error) {
+	id, err := c.Text.Cmd(format, args...)
+	if err != nil {
+		return 0, "", err
+	}
+	c.Text.StartResponse(id)
+	defer c.Text.EndResponse(id)
+	code, msg, err := c.Text.ReadResponse(expectCode)
+	return code, msg, err
+}
+
+// helo sends the HELO greeting to the server. It should be used only when the
+// server does not support ehlo.
+func (c *Client) helo() error {
+	c.ext = nil
+	_, _, err := c.cmd(250, "HELO %s", c.localName)
 	return err
 }
 
-func sendMail(c smtp.Client, from string, to string, msg []byte) error {
-	defer c.Quit()
-	err := c.Mail(from)
+// ehlo sends the EHLO (extended hello) greeting to the server. It
+// should be the preferred greeting for servers that support it.
+func (c *Client) ehlo() error {
+	_, msg, err := c.cmd(250, "EHLO %s", c.localName)
 	if err != nil {
 		return err
 	}
-	err = c.Rcpt(to)
+	ext := make(map[string]string)
+	extList := strings.Split(msg, "\n")
+	if len(extList) > 1 {
+		extList = extList[1:]
+		for _, line := range extList {
+			args := strings.SplitN(line, " ", 2)
+			if len(args) > 1 {
+				ext[args[0]] = args[1]
+			} else {
+				ext[args[0]] = ""
+			}
+		}
+	}
+	if mechs, ok := ext["AUTH"]; ok {
+		c.auth = strings.Split(mechs, " ")
+	}
+	c.ext = ext
+	return err
+}
+
+// StartTLS sends the STARTTLS command and encrypts all further communication.
+// Only servers that advertise the STARTTLS extension support this function.
+func (c *Client) StartTLS(config *tls.Config) error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(220, "STARTTLS")
 	if err != nil {
 		return err
 	}
-	wc, err := c.Data()
+	c.conn = tls.Client(c.conn, config)
+	c.Text = textproto.NewConn(c.conn)
+	c.tls = true
+	return c.ehlo()
+}
+
+// TLSConnectionState returns the client's TLS connection state.
+// The return values are their zero values if StartTLS did
+// not succeed.
+func (c *Client) TLSConnectionState() (state tls.ConnectionState, ok bool) {
+	tc, ok := c.conn.(*tls.Conn)
+	if !ok {
+		return
+	}
+	return tc.ConnectionState(), true
+}
+
+// Verify checks the validity of an email address on the server.
+// If Verify returns nil, the address is valid. A non-nil return
+// does not necessarily indicate an invalid address. Many servers
+// will not verify addresses for security reasons.
+func (c *Client) Verify(addr string) error {
+	if err := validateLine(addr); err != nil {
+		return err
+	}
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(250, "VRFY %s", addr)
+	return err
+}
+
+// Mail issues a MAIL command to the server using the provided email address.
+// If the server supports the 8BITMIME extension, Mail adds the BODY=8BITMIME
+// parameter. If the server supports the SMTPUTF8 extension, Mail adds the
+// SMTPUTF8 parameter.
+// This initiates a mail transaction and is followed by one or more Rcpt calls.
+func (c *Client) Mail(from string) error {
+	if err := validateLine(from); err != nil {
+		return err
+	}
+	if err := c.hello(); err != nil {
+		return err
+	}
+	cmdStr := "MAIL FROM:<%s>"
+	if c.ext != nil {
+		if _, ok := c.ext["8BITMIME"]; ok {
+			cmdStr += " BODY=8BITMIME"
+		}
+		if _, ok := c.ext["SMTPUTF8"]; ok {
+			cmdStr += " SMTPUTF8"
+		}
+	}
+	_, _, err := c.cmd(250, cmdStr, from)
+	return err
+}
+
+// Rcpt issues a RCPT command to the server using the provided email address.
+// A call to Rcpt must be preceded by a call to Mail and may be followed by
+// a Data call or another Rcpt call.
+func (c *Client) Rcpt(to string) error {
+	if err := validateLine(to); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(25, "RCPT TO:<%s>", to)
+	return err
+}
+
+type dataCloser struct {
+	c *Client
+	io.WriteCloser
+}
+
+func (d *dataCloser) Close() error {
+	d.WriteCloser.Close()
+	_, _, err := d.c.Text.ReadResponse(250)
+	return err
+}
+
+// Data issues a DATA command to the server and returns a writer that
+// can be used to write the mail headers and body. The caller should
+// close the writer before calling any more methods on c. A call to
+// Data must be preceded by one or more calls to Rcpt.
+func (c *Client) Data() (io.WriteCloser, error) {
+	_, _, err := c.cmd(354, "DATA")
+	if err != nil {
+		return nil, err
+	}
+	return &dataCloser{c, c.Text.DotWriter()}, nil
+}
+
+// smtpSend connects to the server at addr, switches to TLS if
+// possible, authenticates with the optional mechanism a if possible,
+// and then sends an email from address from, to addresses to, with
+// message msg.
+// The addr must include a port, as in "mail.example.com:smtp".
+//
+// The addresses in the to parameter are the SMTP RCPT addresses.
+//
+// The msg parameter should be an RFC 822-style email with headers
+// first, a blank line, and then the message body. The lines of msg
+// should be CRLF terminated. The msg headers should usually include
+// fields such as "From", "To", "Subject", and "Cc".  Sending "Bcc"
+// messages is accomplished by including an email address in the to
+// parameter but not including it in the msg headers.
+//
+// The smtpSend function and the net/smtp package are low-level
+// mechanisms and provide no support for DKIM signing, MIME
+// attachments (see the mime/multipart package), or other mail
+// functionality. Higher-level packages exist outside of the standard
+// library.
+func smtpSend(addr string, from string, to []string, msg []byte, dial *bool) error {
+	if err := validateLine(from); err != nil {
+		return err
+	}
+	for _, recp := range to {
+		if err := validateLine(recp); err != nil {
+			return err
+		}
+	}
+	c, err := smtpDial(addr)
+	if err != nil {
+		*dial = true
+		return err
+	}
+	defer c.Close()
+	if err = c.hello(); err != nil {
+		return err
+	}
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		config := &tls.Config{ServerName: c.serverName}
+		if err = c.StartTLS(config); err != nil {
+			return err
+		}
+	}
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
 	if err != nil {
 		return err
 	}
-	count, err := wc.Write(msg)
+	_, err = w.Write(msg)
 	if err != nil {
 		return err
 	}
-	if count != len(msg) {
-		return fmt.Errorf("msg len mismatch %d != %d", count, len(msg))
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
+
+// Extension reports whether an extension is support by the server.
+// The extension name is case-insensitive. If the extension is supported,
+// Extension also returns a string that contains any parameters the
+// server specifies for the extension.
+func (c *Client) Extension(ext string) (bool, string) {
+	if err := c.hello(); err != nil {
+		return false, ""
+	}
+	if c.ext == nil {
+		return false, ""
+	}
+	ext = strings.ToUpper(ext)
+	param, ok := c.ext[ext]
+	return ok, param
+}
+
+// Reset sends the RSET command to the server, aborting the current mail
+// transaction.
+func (c *Client) Reset() error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(250, "RSET")
+	return err
+}
+
+// Noop sends the NOOP command to the server. It does nothing but check
+// that the connection to the server is okay.
+func (c *Client) Noop() error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(250, "NOOP")
+	return err
+}
+
+// Quit sends the QUIT command and closes the connection to the server.
+func (c *Client) Quit() error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(221, "QUIT")
+	if err != nil {
+		return err
+	}
+	return c.Text.Close()
+}
+
+// validateLine checks to see if a line has CR or LF as per RFC 5321
+func validateLine(line string) error {
+	if strings.ContainsAny(line, "\n\r") {
+		return errors.New("smtp: A line must not contain CR or LF")
 	}
 	return nil
-}
-
-func encodeRFC2047(str string) string {
-	// use mail's rfc2047 to encode any string
-	addr := mail.Address{Name: str}
-	return strings.Trim(addr.String(), " <>")
-}
-
-func composeMimeMail(to string, from string, subject string, mime string, body string) ([]byte, uint) {
-	var b strings.Builder
-	fmt.Fprintf(&b, "%s: %s\r\n", "From", from)
-	fmt.Fprintf(&b, "%s: %s\r\n", "To", to)
-	fmt.Fprintf(&b, "%s: %s\r\n", "Subject", encodeRFC2047(subject))
-	fmt.Fprintf(&b, "%s: %s\r\n", "MIME-Version", "1.0")
-	fmt.Fprintf(&b, "%s: %s\r\n", "Content-Type", fmt.Sprintf("%s; charset=\"utf-8\"", mime))
-	fmt.Fprintf(&b, "%s: %s\r\n", "Content-Transfer-Encoding", "base64")
-	bytes := []byte(body)
-	b.WriteString("\r\n")
-	b.WriteString(base64.StdEncoding.EncodeToString(bytes))
-	return []byte(b.String()), uint(len(bytes))
 }
